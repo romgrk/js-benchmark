d59065cf8e2a5e125327ccfae31d6cda1504a9b8
Rom Grk <romgrk.cc@gmail.com>
2024-03-17 02:48:36 -0400

    initial commit

---
+++ run.js
@@ -0,0 +1,127 @@
+// Usage: ./run.js <modulePath.js> [--gc]
+
+const timeToRun = 1000
+const pausePerBlock = 500
+
+// const description = {
+//   blocks: [
+//     {
+//       id: 'case-1',
+//       setup: () => {
+//         return () => 2 + 2
+//       }
+//     }
+//   ]
+// }
+
+const args = typeof process !== 'undefined' ? process.argv.slice(2) : ARGV
+const gc =
+  typeof globalThis.gc !== 'undefined' ? globalThis.gc :
+  typeof Bun !== 'undefined' ? Bun.gc : () => {};
+
+const modulePath = args[0]
+const runGC = args.includes('--gc')
+await run(modulePath)
+
+async function run(path) {
+  let blocks
+  try {
+    blocks = await getBlocks(path)
+    await runTests(blocks)
+  } catch(e) {
+    console.log('ERROR: ' + e.message)
+  }
+
+  blocks.forEach(block => {
+    console.log(`CASE: ${(block.id + ':').padEnd(10, ' ')} `
+      + '#'.repeat(block.result.percent / 2).padEnd(50, '.')
+      + ' ' + block.result.percent + '%'
+      + ` (${block.result.amountOfRounds} ops)`
+      + ` (${(block.result.runTime / block.result.amountOfRounds).toPrecision(4)} ms/op)`
+      )
+  })
+}
+
+async function getBlocks(path) {
+  const module = await import(path)
+  const blocks = module.default.blocks
+
+  blocks.forEach((block, index) => {
+    block.run = block.setup()
+  })
+
+  return blocks
+}
+
+async function runTests(blocks) {
+  for (let block of blocks) {
+    runTestForAmountOfTime(block, 500)
+  }
+  await sleep(1000)
+
+  let timeForTest = ((timeToRun + (pausePerBlock * 2)) * blocks.length) - pausePerBlock
+  let completeTestStart = Date.now()
+
+  for (let i = 0; i < blocks.length; i++) {
+    let block = blocks[i]
+    await sleep(pausePerBlock)
+
+    let testResult = runTestForAmountOfTime(block, timeToRun)
+    block.result = {
+      runTime: testResult.runTime,
+      amountOfRounds: testResult.counter,
+      percent: 0
+    }
+    let t = testResult.timer - completeTestStart
+    let progress = Math.round((100 / timeForTest) * t)
+    if (progress > 100) progress = 100
+
+    if (i < blocks.length - 1)
+      await sleep(pausePerBlock)
+  }
+
+  let f = blocks.reduce((prev, current) => (prev.result.amountOfRounds > current.result.amountOfRounds) ? prev : current)
+  const maxRounds = f.result.amountOfRounds
+  for (let block of blocks) {
+    block.result.percent = Math.round(((100 / maxRounds) * block.result.amountOfRounds)*100)/100
+  }
+
+  return blocks
+}
+
+function runTestForAmountOfTime(block, currentTimeToRun = timeToRun) {
+  let startTimer = Date.now()
+  let timer = Date.now()
+  let counter = 0
+  do {
+    block.run()
+    if (runGC)
+      gc()
+    counter++
+    timer = Date.now()
+  } while(timer - startTimer < currentTimeToRun)
+  return { counter, runTime: timer - startTimer, timer }
+}
+
+function sleep(n) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, n)
+  })
+}
+
+function average(xs) {
+  return Math.round((xs.reduce((t, x) => t + x, 0) / (xs.length || 1)) * 100) / 100
+}
+function stddev(xs) {
+  const n = xs.length || 1
+  const mean = xs.reduce((a, b) => a + b) / n
+  return Math.sqrt(xs.map(x => Math.pow(x - mean, 2)).reduce((a, b) => a + b) / n)
+}
+function map(o, fn) {
+  const result = {}
+  for (let key in o) {
+    result[key] = fn(o[key])
+  }
+  return result
+}
+
---
+++ test-object-without-props-loose.js
@@ -0,0 +1,73 @@
+/*
+ * test-object-without-props-loose.js
+ */
+
+const exclude = ["b", "c"];
+const o1 = { a: 1, b: 2, c: 3, d: 4, e: 5 }
+const o2 = { f: 1, b: 2, c: 3, d: 4, e: 5 }
+const o3 = { g: 1, b: 2, c: 3, d: 4, e: 5 }
+const o4 = { h: 1, b: 2, c: 3, d: 4, e: 5 };
+
+export default {
+  blocks: [
+    {
+      id: 'initial',
+      setup: () => {
+        function _objectWithoutPropertiesLoose(source, excluded) {
+          if (source == null)
+            return {};
+          var target = {};
+          var sourceKeys = Object.keys(source);
+          var key, i2;
+          for (i2 = 0; i2 < sourceKeys.length; i2++) {
+            key = sourceKeys[i2];
+            if (excluded.indexOf(key) >= 0)
+              continue;
+            target[key] = source[key];
+          }
+          return target;
+        }
+
+        return () => {
+          let o = null
+          for (let i = 0; i < 10000; i++) {
+            o=_objectWithoutPropertiesLoose(o1, exclude)
+            o=_objectWithoutPropertiesLoose(o2, exclude)
+            o=_objectWithoutPropertiesLoose(o3, exclude)
+            o=_objectWithoutPropertiesLoose(o4, exclude)
+          }
+          return o
+        }
+      }
+    },
+    {
+      id: 'updated',
+      setup: () => {
+        function _objectWithoutPropertiesLoose(source, excluded) {
+          if (source == null)
+            return {};
+          var target = {};
+          for (const key in source) {
+            if (Object.prototype.hasOwnProperty.call(source, key)) {
+              if (excluded.indexOf(key) === -1) {
+                target[key] = source[key]
+              }
+            }
+          }
+          return target;
+        }
+
+        return () => {
+          let o = null
+          for (let i = 0; i < 10000; i++) {
+            o=_objectWithoutPropertiesLoose(o1, exclude)
+            o=_objectWithoutPropertiesLoose(o2, exclude)
+            o=_objectWithoutPropertiesLoose(o3, exclude)
+            o=_objectWithoutPropertiesLoose(o4, exclude)
+          }
+          return o
+        }
+      }
+    }
+  ]
+}
