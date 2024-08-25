1dbd2265a7d9ac59fb861661b6031690bbfc3598
Rom Grk <romgrk.cc@gmail.com>
2024-08-25 12:32:10 -0400

    update

--- benchmarks/hashing.js
+++ benchmarks/hashing.js
@@ -148,59 +148,71 @@ export function murmur2_cached(str, cache) {
 }
 
 
-function rotl32(x, r) {
-  return (x << r) | (x >>> 32 - r)
-}
+const getUint32 = (arr, i) => (arr[i] | arr[i + 1 | 0] << 8 | arr[i + 2 | 0] << 16 | arr[i + 3 | 0] << 24);
+const rotl32 = (x, r) => (x << r) | (x >>> 32 - r);
 
-export function xxh(input) {
-  if (input.length > bufferLength) {
+export function xxh(string) {
+  if (string.length > bufferLength) {
     // buffer.resize() is only available in recent browsers, so we re-allocate
     // a new and views
-    bufferLength = input.length + (4 - input.length % 4)
+    bufferLength = str.length + (4 - str.length % 4)
     buffer = new ArrayBuffer(bufferLength)
 
     uint8View = new Uint8Array(buffer)
     int32View = new Int32Array(buffer)
   }
 
-  const length = encoder.encodeInto(input, uint8View).written;
+  const length8 = encoder.encodeInto(string, uint8View).written;
+
+  const buf = uint8View
 
   const seed = 0;
-  const len = length | 0;
-
+  const len = length8 | 0;
   let i = 0;
   let h = (seed + len | 0) + 0x165667B1 | 0;
 
   if (len < 16) {
-    for (; (i + 3 | 0) < len; i = i + 4 | 0) {
-      h = Math.imul(rotl32(h + Math.imul(int32View[i], 0xC2B2AE3D) | 0, 17), 0x27D4EB2F);
-    }
+    for (; (i + 3 | 0) < len; i = i + 4 | 0)
+      h = Math.imul(rotl32(h + Math.imul(getUint32(buf, i), 0xC2B2AE3D) | 0, 17), 0x27D4EB2F);
   } else {
     let v0 = seed + 0x24234428 | 0;
     let v1 = seed + 0x85EBCA77 | 0;
     let v2 = seed;
     let v3 = seed - 0x9E3779B1 | 0;
 
-    for (; (i + 15 | 0) < len; i = i + 16 | 0) {
-      v0 = Math.imul(rotl32(v0 + Math.imul(int32View[i + 0 | 0], 0x85EBCA77) | 0, 13), 0x9E3779B1);
-      v1 = Math.imul(rotl32(v1 + Math.imul(int32View[i + 4 | 0], 0x85EBCA77) | 0, 13), 0x9E3779B1);
-      v2 = Math.imul(rotl32(v2 + Math.imul(int32View[i + 8 | 0], 0x85EBCA77) | 0, 13), 0x9E3779B1);
-      v3 = Math.imul(rotl32(v3 + Math.imul(int32View[i + 12 | 0], 0x85EBCA77) | 0, 13), 0x9E3779B1);
-    }
+    if (len < 256) {
+      for (; (i + 15 | 0) < len; i = i + 16 | 0) {
+        v0 = Math.imul(rotl32(v0 + Math.imul(getUint32(buf, i + 0 | 0), 0x85EBCA77) | 0, 13), 0x9E3779B1);
+        v1 = Math.imul(rotl32(v1 + Math.imul(getUint32(buf, i + 4 | 0), 0x85EBCA77) | 0, 13), 0x9E3779B1);
+        v2 = Math.imul(rotl32(v2 + Math.imul(getUint32(buf, i + 8 | 0), 0x85EBCA77) | 0, 13), 0x9E3779B1);
+        v3 = Math.imul(rotl32(v3 + Math.imul(getUint32(buf, i + 12 | 0), 0x85EBCA77) | 0, 13), 0x9E3779B1);
+      }
 
-    h = (((rotl32(v0, 1) + rotl32(v1, 7) | 0) + rotl32(v2, 12) | 0) + rotl32(v3, 18) | 0) + len | 0;
-    for (; (i + 3 | 0) < len; i = i + 4 | 0) {
-      h = Math.imul(rotl32(h + Math.imul(int32View[i], 0xC2B2AE3D) | 0, 17), 0x27D4EB2F);
+      h = (((rotl32(v0, 1) + rotl32(v1, 7) | 0) + rotl32(v2, 12) | 0) + rotl32(v3, 18) | 0) + len | 0;
+      for (; (i + 3 | 0) < len; i = i + 4 | 0)
+        h = Math.imul(rotl32(h + Math.imul(getUint32(buf, i), 0xC2B2AE3D) | 0, 17), 0x27D4EB2F);
+    } else {
+      const view = new DataView(buf.buffer);
+      for (; (i + 15 | 0) < len; i = i + 16 | 0) {
+        v0 = Math.imul(rotl32(v0 + Math.imul(view.getUint32(i + 0 | 0, true), 0x85EBCA77) | 0, 13), 0x9E3779B1);
+        v1 = Math.imul(rotl32(v1 + Math.imul(view.getUint32(i + 4 | 0, true), 0x85EBCA77) | 0, 13), 0x9E3779B1);
+        v2 = Math.imul(rotl32(v2 + Math.imul(view.getUint32(i + 8 | 0, true), 0x85EBCA77) | 0, 13), 0x9E3779B1);
+        v3 = Math.imul(rotl32(v3 + Math.imul(view.getUint32(i + 12 | 0, true), 0x85EBCA77) | 0, 13), 0x9E3779B1);
+      }
+
+      h = (((rotl32(v0, 1) + rotl32(v1, 7) | 0) + rotl32(v2, 12) | 0) + rotl32(v3, 18) | 0) + len | 0;
+      for (; (i + 3 | 0) < len; i = i + 4 | 0)
+        h = Math.imul(rotl32(h + Math.imul(view.getUint32(i, true), 0xC2B2AE3D) | 0, 17), 0x27D4EB2F);
     }
   }
 
-  for (; i < len; i = i + 1 | 0) {
-    h = Math.imul(rotl32(h + Math.imul(uint8View[i], 0x165667B1) | 0, 11), 0x9E3779B1);
-  }
+  for (; i < len; i = i + 1 | 0)
+    h = Math.imul(rotl32(h + Math.imul(buf[i], 0x165667B1) | 0, 11), 0x9E3779B1);
   h = Math.imul(h ^ h >>> 15, 0x85EBCA77);
   h = Math.imul(h ^ h >>> 13, 0xC2B2AE3D);
-  return (h ^ h >>> 16) >>> 0;
-};
+
+  return ((h ^ h >>> 16) >>> 0).toString(36);
+}
 
 
 
@@ -261,6 +273,36 @@ export function goober_unmodified(str) {
       out = 11;
   while (i < str.length) out = (101 * out + str.charCodeAt(i++)) >>> 0;
   return 'go' + out;
+}
+
+export function goober_asmjs(str) {
+  let i = 0,
+      out = 11;
+  while (i < str.length) out = (Math.imul(101, out) + str.charCodeAt(i++) | 0) >>> 0;
+  return 'go' + out;
+}
+
+export function goober(string) {
+  let i = 0
+  let out = 11
+
+  if (string.length > bufferLength) {
+    // buffer.resize() is only available in recent browsers, so we re-allocate
+    // a new and views
+    bufferLength = str.length + (4 - str.length % 4)
+    buffer = new ArrayBuffer(bufferLength)
+
+    uint8View = new Uint8Array(buffer)
+    int32View = new Int32Array(buffer)
+  }
+
+  const length = encoder.encodeInto(string, uint8View).written;
+
+  while (i < length) {
+    out = (101 * out + uint8View[i++]) >>> 0
+  }
+
+  return out.toString(36);
 }
 
 export default {
