504d5ef61065cb40a80a8b2e56cedbf36a425ac2
Rom Grk <romgrk.cc@gmail.com>
2024-08-25 13:19:23 -0400

    update

--- benchmarks/hashing.js
+++ benchmarks/hashing.js
@@ -132,6 +132,65 @@ export function murmur2(str) {
   return h.toString(36)
 }
 
+export function murmur2_hex(str) {
+  if (str.length > bufferLength) {
+    // buffer.resize() is only available in recent browsers, so we re-allocate
+    // a new and views
+    bufferLength = str.length + (4 - str.length % 4)
+    buffer = new ArrayBuffer(bufferLength)
+
+    uint8View = new Uint8Array(buffer)
+    int32View = new Int32Array(buffer)
+  }
+
+  const length = encoder.encodeInto(str, uint8View).written;
+
+  // 'm' and 'r' are mixing constants generated offline.
+  // They're not really 'magic', they just happen to work well.
+
+  const m = 0x5bd1e995;
+  const r = 24;
+
+  // Initialize the hash
+  let h = 0
+
+  // Mix 4 bytes at a time into the hash
+
+  let i = 0
+  let len = length
+  for (; len >= 4; i++, len -= 4) {
+    let k = int32View[i]
+
+    k = Math.imul(k, m)
+    k ^= k >>> r
+    k = Math.imul(k, m)
+
+    h = Math.imul(h, m)
+    h ^= k
+  }
+
+  // Handle the last few bytes of the input array
+
+  switch (len) {
+    case 3:
+      h ^= (uint8View[i * 4 + 2] & 0xff) << 16
+    case 2:
+      h ^= (uint8View[i * 4 + 1] & 0xff) << 8
+    case 1:
+      h ^= uint8View[i * 4] & 0xff
+      h = Math.imul(h, m)
+  }
+
+  // Do a few final mixes of the hash to ensure the last few
+  // bytes are well-incorporated.
+
+  h ^= h >>> 13
+  h = Math.imul(h, m)
+  h = (h ^ (h >>> 15)) >>> 0
+
+  return format_hex(h)
+}
+
 export function murmur2_cached(str, cache) {
   const cached = cache.get(str)
   if (cached) {
@@ -146,6 +205,21 @@ export function murmur2_cached(str, cache) {
   cache.set(str, hash)
   return hash
 }
+
+
+const FORMAT_HEX = Array.from({ length: 256 }).map((_, byte) => byte.toString(16).padStart(2, '0'));
+function format_hex(number) {
+  return (
+    FORMAT_HEX[get(number, 24)] +
+    FORMAT_HEX[get(number, 16)] +
+    FORMAT_HEX[get(number,  8)] +
+    FORMAT_HEX[get(number,  0)]
+  );
+}
+function get(n, offset) {
+  return (n >>> offset) & 0xff;
+}
+
 
 
 const getUint32 = (arr, i) => (arr[i] | arr[i + 1 | 0] << 8 | arr[i + 2 | 0] << 16 | arr[i + 3 | 0] << 24);
