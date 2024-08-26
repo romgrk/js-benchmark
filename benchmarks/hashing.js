dad2bb3464c99d0685794f984615efad0e0a3ffd
Rom Grk <romgrk.cc@gmail.com>
2024-08-26 03:55:14 -0400

    refactor: xxh asm.js annotations

--- benchmarks/hashing.js
+++ benchmarks/hashing.js
@@ -222,10 +222,9 @@ function get(n, offset) {
 
 
 
-const getUint32 = (arr, i) => (arr[i] | arr[i + 1 | 0] << 8 | arr[i + 2 | 0] << 16 | arr[i + 3 | 0] << 24);
 const rotl32 = (x, r) => (x << r) | (x >>> 32 - r);
 
-export function xxh(string) {
+export function xxh_unmodified(string) {
   if (string.length > bufferLength) {
     // buffer.resize() is only available in recent browsers, so we re-allocate
     // a new and views
@@ -247,41 +246,87 @@ export function xxh(string) {
 
   if (len < 16) {
     for (; (i + 3 | 0) < len; i = i + 4 | 0)
-      h = Math.imul(rotl32(h + Math.imul(getUint32(buf, i), 0xC2B2AE3D) | 0, 17), 0x27D4EB2F);
+      h = Math.imul(rotl32(h + Math.imul(int32View[i], 0xC2B2AE3D) | 0, 17), 0x27D4EB2F);
   } else {
     let v0 = seed + 0x24234428 | 0;
     let v1 = seed + 0x85EBCA77 | 0;
     let v2 = seed;
     let v3 = seed - 0x9E3779B1 | 0;
 
-    if (len < 256) {
-      for (; (i + 15 | 0) < len; i = i + 16 | 0) {
-        v0 = Math.imul(rotl32(v0 + Math.imul(getUint32(buf, i + 0 | 0), 0x85EBCA77) | 0, 13), 0x9E3779B1);
-        v1 = Math.imul(rotl32(v1 + Math.imul(getUint32(buf, i + 4 | 0), 0x85EBCA77) | 0, 13), 0x9E3779B1);
-        v2 = Math.imul(rotl32(v2 + Math.imul(getUint32(buf, i + 8 | 0), 0x85EBCA77) | 0, 13), 0x9E3779B1);
-        v3 = Math.imul(rotl32(v3 + Math.imul(getUint32(buf, i + 12 | 0), 0x85EBCA77) | 0, 13), 0x9E3779B1);
-      }
-
-      h = (((rotl32(v0, 1) + rotl32(v1, 7) | 0) + rotl32(v2, 12) | 0) + rotl32(v3, 18) | 0) + len | 0;
-      for (; (i + 3 | 0) < len; i = i + 4 | 0)
-        h = Math.imul(rotl32(h + Math.imul(getUint32(buf, i), 0xC2B2AE3D) | 0, 17), 0x27D4EB2F);
-    } else {
-      const view = new DataView(buf.buffer);
-      for (; (i + 15 | 0) < len; i = i + 16 | 0) {
-        v0 = Math.imul(rotl32(v0 + Math.imul(view.getUint32(i + 0 | 0, true), 0x85EBCA77) | 0, 13), 0x9E3779B1);
-        v1 = Math.imul(rotl32(v1 + Math.imul(view.getUint32(i + 4 | 0, true), 0x85EBCA77) | 0, 13), 0x9E3779B1);
-        v2 = Math.imul(rotl32(v2 + Math.imul(view.getUint32(i + 8 | 0, true), 0x85EBCA77) | 0, 13), 0x9E3779B1);
-        v3 = Math.imul(rotl32(v3 + Math.imul(view.getUint32(i + 12 | 0, true), 0x85EBCA77) | 0, 13), 0x9E3779B1);
-      }
-
-      h = (((rotl32(v0, 1) + rotl32(v1, 7) | 0) + rotl32(v2, 12) | 0) + rotl32(v3, 18) | 0) + len | 0;
-      for (; (i + 3 | 0) < len; i = i + 4 | 0)
-        h = Math.imul(rotl32(h + Math.imul(view.getUint32(i, true), 0xC2B2AE3D) | 0, 17), 0x27D4EB2F);
+    for (; (i + 15 | 0) < len; i = i + 16 | 0) {
+      v0 = Math.imul(rotl32(v0 + Math.imul(int32View[i + 0 | 0], 0x85EBCA77) | 0, 13), 0x9E3779B1);
+      v1 = Math.imul(rotl32(v1 + Math.imul(int32View[i + 4 | 0], 0x85EBCA77) | 0, 13), 0x9E3779B1);
+      v2 = Math.imul(rotl32(v2 + Math.imul(int32View[i + 8 | 0], 0x85EBCA77) | 0, 13), 0x9E3779B1);
+      v3 = Math.imul(rotl32(v3 + Math.imul(int32View[i + 12 | 0], 0x85EBCA77) | 0, 13), 0x9E3779B1);
     }
+
+    h = (((rotl32(v0, 1) + rotl32(v1, 7) | 0) + rotl32(v2, 12) | 0) + rotl32(v3, 18) | 0) + len | 0;
+    for (; (i + 3 | 0) < len; i = i + 4 | 0)
+      h = Math.imul(rotl32(h + Math.imul(int32View[i], 0xC2B2AE3D) | 0, 17), 0x27D4EB2F);
   }
 
   for (; i < len; i = i + 1 | 0)
     h = Math.imul(rotl32(h + Math.imul(buf[i], 0x165667B1) | 0, 11), 0x9E3779B1);
+  h = Math.imul(h ^ h >>> 15, 0x85EBCA77);
+  h = Math.imul(h ^ h >>> 13, 0xC2B2AE3D);
+
+  return ((h ^ h >>> 16) >>> 0).toString(36);
+}
+
+export function xxh(string) {
+  if (string.length > bufferLength) {
+    // buffer.resize() is only available in recent browsers, so we re-allocate
+    // a new and views
+    bufferLength = string.length + (4 - string.length % 4)
+    buffer = new ArrayBuffer(bufferLength)
+
+    uint8View = new Uint8Array(buffer)
+    int32View = new Int32Array(buffer)
+  }
+
+  const length8 = encoder.encodeInto(string, uint8View).written;
+
+  const buf = uint8View
+
+  const seed = 0;
+  const len = length8 | 0;
+  let i = 0;
+  let h = (seed + len | 0) + 0x165667B1 | 0;
+
+  if (len < 16) {
+    for (; (i + 3 | 0) < len; i = i + 4 | 0)
+      h = Math.imul(
+        rotl32(h + Math.imul(int32View[i] | 0, 0xC2B2AE3D) | 0, 17) | 0,
+        0x27D4EB2F
+      );
+  } else {
+    let v0 = seed + 0x24234428 | 0;
+    let v1 = seed + 0x85EBCA77 | 0;
+    let v2 = seed;
+    let v3 = seed - 0x9E3779B1 | 0;
+
+    for (; (i + 15 | 0) < len; i = i + 16 | 0) {
+      v0 = Math.imul(rotl32(v0 + Math.imul(int32View[i + 0 | 0]  | 0, 0x85EBCA77) | 0, 13) | 0, 0x9E3779B1);
+      v1 = Math.imul(rotl32(v1 + Math.imul(int32View[i + 4 | 0]  | 0, 0x85EBCA77) | 0, 13) | 0, 0x9E3779B1);
+      v2 = Math.imul(rotl32(v2 + Math.imul(int32View[i + 8 | 0]  | 0, 0x85EBCA77) | 0, 13) | 0, 0x9E3779B1);
+      v3 = Math.imul(rotl32(v3 + Math.imul(int32View[i + 12 | 0] | 0, 0x85EBCA77) | 0, 13) | 0, 0x9E3779B1);
+    }
+
+    h = (((
+        rotl32(v0,  1) | 0 +
+        rotl32(v1,  7) | 0) +
+        rotl32(v2, 12) | 0) +
+        rotl32(v3, 18) | 0)
+    + len | 0;
+
+    for (; (i + 3 | 0) < len; i = i + 4 | 0) {
+      h = Math.imul(rotl32(h + Math.imul(int32View[i], 0xC2B2AE3D) | 0, 17) | 0, 0x27D4EB2F);
+    }
+  }
+
+  for (; i < len; i = i + 1 | 0) {
+    h = Math.imul(rotl32(h + Math.imul(buf[i], 0x165667B1) | 0, 11) | 0, 0x9E3779B1);
+  }
   h = Math.imul(h ^ h >>> 15, 0x85EBCA77);
   h = Math.imul(h ^ h >>> 13, 0xC2B2AE3D);
 
@@ -368,26 +413,39 @@ export function goober(str) {
 
 export default {
   blocks: [
-    {
-      id: 'murmur2_unmodified',
-      setup: () => {
-        return () => {
-          for (let i = 0; i < inputs.length; i++) {
-            murmur2_unmodified(inputs[i])
-          }
-        }
-      }
-    },
-    {
-      id: 'murmur2',
-      setup: () => {
-        return () => {
-          for (let i = 0; i < inputs.length; i++) {
-            murmur2(inputs[i])
-          }
-        }
-      }
-    },
+    // {
+    //   id: 'murmur2_unmodified',
+    //   setup: () => {
+    //     return () => {
+    //       for (let i = 0; i < inputs.length; i++) {
+    //         murmur2_unmodified(inputs[i])
+    //       }
+    //     }
+    //   }
+    // },
+    //
+    // {
+    //   id: 'murmur2',
+    //   setup: () => {
+    //     return () => {
+    //       for (let i = 0; i < inputs.length; i++) {
+    //         murmur2(inputs[i])
+    //       }
+    //     }
+    //   }
+    // },
+    //
+    // {
+    //   id: 'murmur2_hex',
+    //   setup: () => {
+    //     return () => {
+    //       for (let i = 0; i < inputs.length; i++) {
+    //         murmur2_hex(inputs[i])
+    //       }
+    //     }
+    //   }
+    // },
+
 
     // {
     //   id: 'murmur2_cached (MUI dataset)',
@@ -423,29 +481,38 @@ export default {
       }
     },
     {
-      id: 'fnv-1a',
+      id: 'xxh_unmodified',
       setup: () => {
         return () => {
-          for (let i = 0; i < inputs.length; i++) { fnv1a(inputs[i]) }
+          for (let i = 0; i < inputs.length; i++) { xxh_unmodified(inputs[i]) }
         }
       }
     },
-    {
-      id: 'djb2a',
-      setup: () => {
-        return () => {
-          for (let i = 0; i < inputs.length; i++) { djb2a(inputs[i]) }
-        }
-      }
-    },
-    {
-      id: 'goober_unmodified',
-      setup: () => {
-        return () => {
-          for (let i = 0; i < inputs.length; i++) { goober_unmodified(inputs[i]) }
-        }
-      }
-    },
+
+    // {
+    //   id: 'fnv-1a',
+    //   setup: () => {
+    //     return () => {
+    //       for (let i = 0; i < inputs.length; i++) { fnv1a(inputs[i]) }
+    //     }
+    //   }
+    // },
+    // {
+    //   id: 'djb2a',
+    //   setup: () => {
+    //     return () => {
+    //       for (let i = 0; i < inputs.length; i++) { djb2a(inputs[i]) }
+    //     }
+    //   }
+    // },
+    // {
+    //   id: 'goober_unmodified',
+    //   setup: () => {
+    //     return () => {
+    //       for (let i = 0; i < inputs.length; i++) { goober_unmodified(inputs[i]) }
+    //     }
+    //   }
+    // },
   ]
 }
 
