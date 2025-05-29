46c2d0940f50f581f22ca76fc482ba235aeef6ed
Rom Grk <romgrk.cc@gmail.com>
2025-05-29 19:06:29 -0400

    lint

---
+++ benchmarks/object-shallow-compare.js
@@ -0,0 +1,116 @@
+
+const nObjects = 64
+
+function generateMono(nKeys, generate) {
+  return Array.from({ length: nObjects }).map(() =>
+    Array.from({ length: nKeys }).reduce((acc, curr, index) => {
+      acc['key' + index] = generate(index)
+      return acc
+    }, {})
+  )
+}
+
+const generate = process.argv[4] === 'equal' ? (index) => index : () => Math.random()
+
+const nKeys = process.argv[3] ? +process.argv[3] : 15
+
+const inputs = generateMono(nKeys, generate)
+
+
+function shallowDiffers(a, b) {
+  for (let i in a) if (i !== '__source' && !(i in b)) return true;
+  for (let i in b) if (i !== '__source' && a[i] !== b[i]) return true;
+  return false;
+}
+
+function shallowDiffers_reverse(a, b) {
+	for (let i in a) if (i !== '__source' && a[i] !== b[i]) return true;
+	for (let i in b) if (i !== '__source' && !(i in a)) return true;
+	return false;
+}
+
+function fastObjectShallowEqual(a, b) {
+  let aLength = 0;
+  let bLength = 0;
+
+  for (const key in a) {
+    if (key === '__source') { continue }
+
+    aLength += 1;
+
+    if (a[key] !== b[key]) {
+      return false;
+    }
+    if (!(key in b)) {
+      return false;
+    }
+  }
+
+  for (const key in b) {
+    if (key === '__source') { continue }
+    bLength += 1;
+  }
+  return aLength === bLength;
+}
+
+export default {
+  blocks: [
+    {
+      id: 'shallowDiffers',
+      setup: () => {
+        const a = inputs[0]
+
+        return () => {
+          let result = 0
+
+          for (let i = 0; i < inputs.length - 1; i++) {
+            const b = inputs[i]
+            if (!shallowDiffers(a, b)) {
+              result += 1
+            }
+          }
+
+          return result
+        }
+      }
+    },
+    {
+      id: 'shallowDiffers_reverse',
+      setup: () => {
+        const a = inputs[0]
+
+        return () => {
+          let result = 0
+
+          for (let i = 0; i < inputs.length - 1; i++) {
+            const b = inputs[i]
+            if (!shallowDiffers_reverse(a, b)) {
+              result += 1
+            }
+          }
+
+          return result
+        }
+      }
+    },
+    {
+      id: 'fastObjectShallowEqual',
+      setup: () => {
+        const a = inputs[0]
+
+        return () => {
+          let result = 0
+
+          for (let i = 0; i < inputs.length - 1; i++) {
+            const b = inputs[i]
+            if (fastObjectShallowEqual(a, b)) {
+              result += 1
+            }
+          }
+
+          return result
+        }
+      }
+    },
+  ]
+}
